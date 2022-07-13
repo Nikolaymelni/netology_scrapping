e5a10310cae82ae727cc3612a4364a5e1ca3aa8d
Николай Мельниченко <nikolaymelni@gmail.com>
2022-07-13 21:03:53 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+def habr_posts(keywords):
+    responce = requests.get('https://habr.com/ru/all/')
+    responce.raise_for_status()
+    soup = BS(responce.text, 'html.parser')
+    posts = soup.find_all('article', class_='tm-articles-list__item')
+
+    for post in posts:
+        post_title = post.find('a', class_='tm-article-snippet__title-link')
+        link = f"https://habr.com{post_title.attrs.get('href')}"
+        res_article = requests.get(link)
+        soup_article = BS(res_article.text, 'html.parser')
+        article = soup_article.find('div', id="post-content-body")
+        if set(keywords) & set(post.text.lower().split(' ')) or set(keywords) & set(article.text.lower().split(' ')):
+            post_time = post.find('time')
+            print(f"{post_time.attrs.get('title')}, {post_title.text}, https://habr.com{post_title.attrs.get('href')}")
+
+
+if __name__ == '__main__':
+    print(habr_posts(['дизайн', 'фото', 'web', 'python']))
